8c92194a5817b2aec57f4eff772557ed70b88e76
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-27 22:18:30 +0200

    Added `TokenizerIterationInterface`

---
+++ src/TokenizerIterationInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dhii\Parser\Tokenizer;
+
+use Dhii\Iterator\IterationInterface;
+
+/**
+ * An iteration of a tokenizer.
+ *
+ * @since [*next-version*]
+ */
+interface TokenizerIterationInterface extends
+        IterationInterface,
+        TokenAwareInterface
+{
+}
