f85c214bf05ad983cbb587ede31091e368115435
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-27 22:14:15 +0200

    Tightened test for `TokenizerInterface`

--- test/unit/TokenizerInterfaceTest.php
+++ test/unit/TokenizerInterfaceTest.php
@@ -52,5 +52,6 @@ class TokenizerInterfaceTest extends TestCase
         $subject = $this->createInstance();
 
         $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the test subject could not be created');
+        $this->assertInstanceOf('Dhii\Iterator\IteratorInterface', $subject, 'Subject does not implement required interface');
     }
 }
